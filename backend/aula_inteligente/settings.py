08bc0148a692983103050516bddf48e48ce065c8
Oerlinker <oerlinker@gmail.com>
2025-05-24 16:55:42 -0400

    CORS 1.0.7

--- backend/aula_inteligente/settings.py
+++ backend/aula_inteligente/settings.py
@@ -158,10 +158,8 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "https://example.com",
-#     "https://sub.example.com",
-#     "http://localhost:3000",
-# ]
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8081",
+    "http://192.168.100.53:8081"
+]
